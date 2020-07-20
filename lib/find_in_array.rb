f9c890f2aa28f8f1dd0294547ae3192bf21fc646
Luis Garcia <l.garcia24@hotmail.com>
2020-07-20 06:45:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,6 +2,9 @@ def find_element_index(array, value_to_find)
   count = 0
   
   while count < array.length do
-    if 
+    if array[count] == value_to_find
+      return array[count]
+    end
+    count +=1
   end
 end
